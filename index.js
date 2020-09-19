28e32303e62b289796f2730a92b8f2fb97aae97b
PiyushMewada <piyush.mewada1@gmail.com>
2020-09-19 14:51:14 -0500

    changed randomization for BRUH

--- index.js
+++ index.js
@@ -308,19 +308,19 @@ client.on("message", async msg => {
 			bruhCount++
 			//Pick a random bruh reaction
 			
-			/*const randBruh = Math.random()
-			if(randBruh < .25) {*/
+			const randBruh = Math.random()
+			if(randBruh < .83) {
 				msg.react("🇧")
 				msg.react("🇷")
 				msg.react("🇺")
 				msg.react("🇭")
-			/*} else if (randBruh < .5) {
+			} else if (randBruh < .89) {
 				msg.channel.send("𝓑 𝓡 𝓤 𝓗")
-			} else if (randBruh < .75) {
+			} else if (randBruh < .95) {
 				msg.channel.send("𝔹 ℝ 𝕌 ℍ")
 			} else {
 				msg.channel.send("***B R U H***")
-			}*/
+			}
 	}
 
 	//If message says Good Night reply with a goodbye message
